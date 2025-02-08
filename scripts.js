3cb0c409dcb4e29796d3f627c393de372ab46637
kleivertales <kleiver@ymail.com>
2025-02-08 13:03:26 -0300

    alteracoes

--- scripts.js
+++ scripts.js
@@ -4,8 +4,14 @@ function generateNumber() {
 
 const  min  = Math.ceil (document.querySelector(".input-min").value)
 const  max = Math.floor (document.querySelector(".input-max").value)
+
+if (min >= max){
+    alert("O valor minimo deve ser menor  que o valor maximo ")
+}
+
+else {
+
 const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
 alert(result)
-
+}
 }
